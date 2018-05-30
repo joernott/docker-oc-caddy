cceb5edbe106305714f5646926d442415ea1d76b
Joern Ott <joern.ott@ott-consult.de>
2018-05-30 18:56:16 +0000

    Validate checksum

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ PARENT_HISTORY="scratch"
 patch_dockerfile
 cd src
 curl -jksSL 'http://mgmt0.be.ott-consult.de/repo/caddy/caddy' -o caddy
+curl -jksSL 'http://mgmt0.be.ott-consult.de/repo/caddy/caddy.sha256' -o caddy.sha256
 chmod 0777 caddy
+sha256sum -c caddy.sha256
 cd ..
 docker build -t registry.ott-consult.de/oc/caddy .
 docker push registry.ott-consult.de/oc/caddy
